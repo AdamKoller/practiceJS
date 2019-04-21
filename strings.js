ebbf6cf0e4f69f14399c5beca603caf08df9f44d
Adam Koller <41529662+AdamKoller@users.noreply.github.com>
2019-04-20 22:22:46 -0500

    Delete strings.js

--- strings.js
+++
@@ -1,7 +0,0 @@
-function wordBlanks(myNoun,myAdjective) {
-var result = "";
-result += "My " + myNoun + " is really " + myAdjective + ".";
-return result;
-}
-document.write(wordBlanks('dog','nice'));
-
